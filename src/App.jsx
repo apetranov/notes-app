62db22356bcbaa7cea3dfda94650b74fd75ee544
Alexander Petranov <shasho68.5@gmail.com>
2025-11-27 15:52:40 +0200

    Remove unneeded console.log

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     const notesCopy = [...notes];
     notesCopy.push(newNote);
     setNotes(notesCopy);
-    console.log(notes);
+    
     setTitle("");
     setContent("");
   }
